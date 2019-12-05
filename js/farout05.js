3649c9efef891feb35620db368f85bfdc1350b34
Ji Yun <j.kim@cca.edu>
2019-12-05 15:19:31 -0800

    done
    done

---
+++ js/farout05.js
@@ -0,0 +1,39 @@
+ $(function() {
+  var body = $('#starshine'),
+      template = $('.template.shine'),
+      stars =  500,
+      sparkle = 20;
+  
+    
+  var size = 'small';
+  var createStar = function() {
+    template.clone().removeAttr('id').css({
+      top: (Math.random() * 100) + '%',
+      left: (Math.random() * 100) + '%',
+      webkitAnimationDelay: (Math.random() * sparkle) + 's',
+      mozAnimationDelay: (Math.random() * sparkle) + 's'
+    }).addClass(size).appendTo(body);
+  };
+ 
+  for(var i = 0; i < stars; i++) {
+    if(i % 2 === 0) {
+      size = 'small';
+    } else if(i % 3 === 0) {
+      size = 'medium';
+    } else {
+      size = 'large';
+    }
+    
+    createStar();
+  }
+
+  let floating = $('.floating')
+  floating(function(){
+    
+
+
+    setTimeout(function(){
+      $(document.body).addClass('animationsDone')      
+    },16000)
+  })
+});
